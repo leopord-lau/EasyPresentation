55333d60196aef55ff750b770a2b1d40cace3e2f
leo <2279846752@qq.com>
2022-02-11 21:20:05 +0800

    modify debounce.js

--- src/debounce.js
+++ src/debounce.js
@@ -1,4 +1,5 @@
-const debounce = (fn, delay) => {
+// 非立即执行
+const debounce1 = (fn, delay) => {
   let timer = null;
   return (...args) => {
     if(timer) clearTimeout(timer);
@@ -8,6 +9,49 @@ const debounce = (fn, delay) => {
   }
 }
 
-document.body.addEventListener('click', debounce(()=> {
+// 立即执行
+const debounce2 = (fn, delay) => {
+  let timer = null;
+  let emitNow = true;
+  return (...args) => {
+    if(timer) clearTimeout(timer);
+    if(emitNow) {
+      fn.apply(this, args);
+      emitNow = false;
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+        emitNow = true;
+      }, delay)
+    }
+  }
+}
+
+// 通过参数控制是否立即执行
+const debounce3 = (fn, delay, isImmediate) => {
+  let timer = null;
+  let emitNow = true;
+  return (...args) => {
+    if(timer) clearTimeout(timer);
+    
+    if(isImmediate) {
+      if(emitNow) {
+        fn.apply(this, args);
+        emitNow = false;
+      } else {
+        timer = setTimeout(() => {
+          fn.apply(this, args);
+          emitNow = true;
+        }, delay)
+      }
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+      }, delay)
+    }
+  }
+}
+
+document.body.addEventListener('click', debounce3(()=> {
   console.log('log')
-}, 1000))
+}, 2000))
